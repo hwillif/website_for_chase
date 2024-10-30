6e144d49bf5e826962de9733a6963774ce9d1f9a
Hayden Williford <123603483+hwillif@users.noreply.github.com>
2024-10-29 22:18:52 -0400

    Update test.py

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ wet = 'fart-9-228245.mp3'
 spicy = 'fart-8-228244.mp3'
 shart = 'unusual-sounding-fart-249278.mp3'
 
-fart = st.selectbox("What kind of fart do you want?",('None :/ I hate Winston','Dry', 'Wet', 'Spicy','Shart'))
+fart = st.selectbox("What kind of fart do you want?",('None :/ I hate Winston','Dry', 'Wet', 'Spicy','Shart','Surprise'))
 if fart == "Wet":
     st.audio(wet,loop=True,autoplay=True)
 if fart == "Dry":
@@ -28,6 +28,8 @@ if fart == "Spicy":
     st.audio(spicy,loop=True,autoplay=True)
 if fart == "Shart":
     st.audio(shart,loop=True,autoplay=True)
+if fart == "Surprise":
+    st.audio(surprise,loop=True,autoplay=True)
 
 st.write("This is Winston")
 st.image(Rotated_image)
