1cb7cfdd22663b665e6e0d8322b816e9f5c91f40
Hayden Williford <123603483+hwillif@users.noreply.github.com>
2024-10-29 21:11:01 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,10 @@
 import altair as alt
 import pandas as pd
 import streamlit as st
+from PIL import Image
 
-import cv2  # importing cv 
-import imutils 
-    
-# read an image as input using OpenCV 
-image = cv2.imread(r'IMG_8252.jpg') 
-  
-Rotated_image = imutils.rotate(image, angle=0)
+img = Image.open(img)
+Rotated_image = img.rotate(270)
 
 st.write("This is my website")
 st.audio('fart-9-228245.mp3')
